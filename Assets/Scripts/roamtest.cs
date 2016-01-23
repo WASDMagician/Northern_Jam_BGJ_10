d1ed695f94690b9b73363eafcf7a057ea2dfcabc
Elit3d <kyle.j.t@live.co.uk>
2016-01-23 17:58:47 +0000

    improved enemy roaming - chase = broken

--- Assets/Scripts/roamtest.cs
+++ Assets/Scripts/roamtest.cs
@@ -28,6 +28,7 @@ public class roamtest : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.T))
         {
             playerSpotted = !playerSpotted;
+            //agent.SetDestination(startPosition);
         }
 
         if (!playerSpotted)
@@ -36,6 +37,8 @@ public class roamtest : MonoBehaviour {
         }
         else if (playerSpotted)
         {
+            //broken right now wont return to original starting point
+
             Chase();
         }
     }
@@ -43,18 +46,20 @@ public class roamtest : MonoBehaviour {
     void Roam()
     {
         Vector3 position = transform.position;
-
-        if(roamPositions != null && roamPositions.Count > 1)
+        agent.speed = 1.0f;
+        if (roamPositions != null && roamPositions.Count > 1)
         {
             Vector3 destination = roamPositions[roamPositionIndex];
 
-            if(destination != position && currentPosition != destination)
+            Debug.Log("destination " + destination + " position " + position);
+
+            if (destination != position && currentPosition != destination)
             { 
                 agent.SetDestination(destination);
                 currentPosition = destination;
                 isAtPosition = false;
             }
-            else if (!isAtPosition)
+            else if (position == destination && !isAtPosition)
             {
                 isAtPosition = true;
                 //random timer on wait
@@ -63,6 +68,7 @@ public class roamtest : MonoBehaviour {
         }
         else if(position != startPosition)
         {
+            Debug.Log("not in start pos");
             agent.SetDestination(startPosition);
         }
     }
@@ -70,6 +76,7 @@ public class roamtest : MonoBehaviour {
     void Chase()
     {
         Vector3 playerPosition = player_controller.transform.position;
+        agent.speed = 2.0f;
         // Enemy needs to lose interest
         agent.SetDestination(playerPosition);
     }
