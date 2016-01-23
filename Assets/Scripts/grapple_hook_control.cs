90a3a10e874ee5a222bfd75ef04e1872359fa19f
WASDMagician <WASDMagician@gmail.com>
2016-01-23 17:25:28 +0000

    .

--- Assets/Scripts/grapple_hook_control.cs
+++ Assets/Scripts/grapple_hook_control.cs
@@ -1,76 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class grapple_hook_control : MonoBehaviour {
+public class grapple_hook_control : MonoBehaviour
+{
 
+    private Rigidbody body;
+
+    public float max_distance;
     public float speed;
     public bool has_fired;
-    public float snap_dist;
-    public float max_length;
-    private Rigidbody body;
+    public float snap_distance;
     bool extending;
     bool retracting;
 
-
-    Vector3 start_position;
-    Vector3 gun_position;
-    CharacterController controller;
-
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         body = GetComponent<Rigidbody>();
         has_fired = false;
-        start_position = transform.position;
-        gun_position = transform.parent.transform.position;
-        controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(has_fired)
+        extending = false;
+        retracting = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+        if (has_fired)
         {
             Move();
         }
-	}
+    }
 
     public void Fire()
     {
-        has_fired = true;
-        start_position = transform.position;
-        retracting = false;
-        extending = true;
+        if (has_fired == false)
+        {
+            has_fired = true;
+            extending = true;
+            retracting = false;
+        }
     }
 
     void Move()
     {
-        float distance = Vector3.Distance(transform.position, transform.parent.transform.position);
-        print(distance);
-        if(distance < max_length && extending)
+        float distance = Vector3.Distance(transform.position, transform.parent.position);
+
+        Vector3 initial = transform.position;
+        Vector3 move_by = initial;
+        Vector3 end_position = initial;
+
+        if (extending == true && distance < max_distance)
         {
-            body.MovePosition(transform.position + transform.forward * (speed * Time.deltaTime));
+            print("Extending");
+            move_by = transform.forward * (speed * Time.deltaTime);
+            end_position = initial + move_by;
         }
-        else if(distance > max_length && extending)
+        else if (extending == true && distance >= max_distance)
         {
+            print("Switching");
             extending = false;
             retracting = true;
         }
-
-        if (retracting && distance > snap_dist)
+        else if (retracting == true && distance > snap_distance)
         {
-            body.MovePosition(transform.position - transform.forward * (speed * Time.deltaTime));
+            print("Retracting");
+            move_by = -transform.forward * (speed * Time.deltaTime);
+            end_position = initial + move_by;
         }
-        else if(retracting && distance <= snap_dist)
+
+        transform.position = end_position;
+
+        if (retracting == true && distance <= snap_distance)
         {
-            transform.position = transform.parent.transform.position;
-            has_fired = false;
             extending = false;
             retracting = false;
+            has_fired = false;
+            transform.position = transform.parent.transform.position;
         }
     }
 
     void OnTriggerEnter(Collider col)
     {
-        extending = false;
-        retracting = true;
+        if (!col.CompareTag("Gun"))
+        {
+            if (extending == true)
+            {
+                extending = false;
+                retracting = true;
+            }
+        }
     }
+
+
 }
