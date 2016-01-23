859dcea66461b1acf73f09293be561314c067589
Elit3d <kyle.j.t@live.co.uk>
2016-01-23 14:03:06 +0000

    Enemy roaming
    Enemy can now roam with our own set paths

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,65 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public int health;
+    private bool alerted;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform target;
+    NavMeshAgent nmAgent;
+    //private Vector3 startPos; //starting position
+   // private float roamSpeed;
+   // public float wanderRadius;
+
+    // Use this for initialization
+    void Start() {
+        alerted = false;
+        nmAgent = GetComponent<NavMeshAgent>();
+       // startPos = transform.position;
+       // roamSpeed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Alert();
+
+        nmAgent.SetDestination(target.position);
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    // Possibly dont need this as we are setting health in hierarchy 
+    public void setHealth(int h)
+    {
+        health = h;
+    }
+
+    // Enemy take damage
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public bool getAlerted()
+    {
+        return alerted;
+    }
+
+    public void setAlerted(bool alert)
+    {
+        alerted = alert;
+    }
+
+    // Alert other guards
+    public void Alert()
+    {
+        //alerted = true;
+        // some more logic
+        if(getAlerted() == true)
+        {
+            Debug.Log("Alerting AI...");
+            //do stuff
+        }
+    }
 }
---
+++ Assets/Scripts/roamtest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class roamtest : MonoBehaviour {
+
+    //public Transform target;
+    NavMeshAgent agent;
+    private Vector3 startPosition;
+    public List<Vector3> roamPositions;
+    private int roamPositionIndex = 0;
+    private Vector3 currentPosition;
+    public bool isAtPosition = false;
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        startPosition = transform.position;
+	}
+	
+	void Update () {
+        //agent.SetDestination(target.position);
+        Roam();
+	}
+
+    void Roam()
+    {
+        Vector3 position = transform.position;
+
+        if(roamPositions != null && roamPositions.Count > 1)
+        {
+            Vector3 destination = roamPositions[roamPositionIndex];
+
+            if(destination != position && currentPosition != destination)
+            { 
+                agent.SetDestination(destination);
+                currentPosition = destination;
+                isAtPosition = false;
+            }
+            else if (!isAtPosition)
+            {
+                isAtPosition = true;
+                StartCoroutine(Wait(5.0f));
+            }
+        }
+        else if(position != startPosition)
+        {
+            agent.SetDestination(startPosition);
+        }
+    }
+
+    IEnumerator Wait(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (roamPositionIndex < roamPositions.Count - 1)
+        {
+            roamPositionIndex++;
+        }
+        else
+        {
+            roamPositionIndex = 0;
+        }
+        isAtPosition = false;
+    }
+}
