71756900fa45be7bb59a3bb3e8a593317576826a
WASDMagician <WASDMagician@gmail.com>
2016-01-23 23:49:51 +0000

    blah

--- Assets/Scripts/grapple_hook_control.cs
+++ Assets/Scripts/grapple_hook_control.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class grapple_hook_control : MonoBehaviour
 {
+    
     Vector3 last_position;
     private Rigidbody body;
     public GameObject parent_object;
     public GameObject grabbed_object;
-
+    Transform parent_pos;
+    float distance;
+    Vector3 hook_pos;
     public float max_distance;
     public float speed;
     public bool has_fired;
@@ -38,7 +41,7 @@ public class grapple_hook_control : MonoBehaviour
             pull_shot = true;
             Fire();
         }
-        else if(Input.GetButton("Fire2"))
+        else if (Input.GetButton("Fire2"))
         {
             hook_shot = true;
             pull_shot = false;
@@ -63,79 +66,82 @@ public class grapple_hook_control : MonoBehaviour
 
     void Move()
     {
-        float distance = Vector3.Distance(transform.position, transform.parent.position);
-        print(distance);
-        Vector3 initial = transform.position;
-        Vector3 move_by = initial;
-        Vector3 end_position = initial;
-
-        if (extending == true && distance < max_distance)
+        if(extending)
         {
-            print("Extending");
-            move_by = transform.forward * (speed * Time.deltaTime);
-            end_position = initial + move_by;
+            Extend();
         }
-        else if (extending == true && distance >= max_distance)
+        if(retracting)
         {
-            print("Switching");
+            if(grabbed_object != null && pull_shot)
+            {
+                Pull_Object();
+            }
+            else if(grabbed_object != null && hook_shot)
+            {
+                Pull_Player();
+            }
+            else if(grabbed_object == null)
+            {
+                Retract();
+            }
+        }
+    }
+
+    void Extend()
+    {
+        parent_pos = transform.parent.transform;
+        distance = Vector3.Distance(transform.position, parent_pos.position);
+        transform.position += parent_pos.forward * (speed * Time.deltaTime);
+
+        if(distance > max_distance)
+        {
             extending = false;
             retracting = true;
         }
-        else if (retracting == true && distance > snap_distance)
-        {
-            print("Retracting");
-            move_by = -transform.forward * (speed * Time.deltaTime);
-            end_position = initial + move_by;
-        }
+        
+    }
 
-        if (pull_shot || extending)
-        {
-            transform.position = end_position; //moves hook back toward player
-        }
-        if(retracting && hook_shot)
-        {
-            transform.position = end_position; //moves hook back to player
-            parent_object.transform.position = parent_object.transform.position - move_by; //moves player toward object
-        }
+    void Retract()
+    {
+        parent_pos = transform.parent.transform;
+        distance = Vector3.Distance(transform.position, parent_pos.position);
+        transform.position -= parent_pos.forward * (speed * Time.deltaTime);
 
-        if(retracting == true && hook_shot == true && distance < hook_distance)
+        if (distance < hook_distance)
         {
             extending = false;
             retracting = false;
             has_fired = false;
-
-            grabbed_object.transform.parent = null;
-
-
-            transform.position = transform.parent.transform.position;
+            grabbed_object = null;
+            transform.position = parent_pos.position;
         }
+    }
 
-        if (retracting == true && distance <= snap_distance)
-        {
-            extending = false;
-            retracting = false;
-            has_fired = false;
+    void Pull_Object()
+    {
+        Retract();
+        grabbed_object.transform.position = transform.position;
+    }
 
-            grabbed_object.transform.parent = null;
-
-            transform.position = transform.parent.transform.position;
-        }
+    void Pull_Player()
+    {
+        //distance is between player object and grabbed object
+        //target is grabbed object
+        //move player object toward grabbed object
+        //parent_object.transform.position += new Vector3(0, 10, 0);
+        parent_object.transform.position += grabbed_object.transform.forward * (speed * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider col)
     {
-        if (!col.CompareTag("Gun"))
+        if(!col.CompareTag("Gun"))
         {
-            if (extending == true)
+            if(extending)
             {
-                col.transform.parent = this.transform;
+                grabbed_object = col.gameObject;
+                hook_pos = grabbed_object.transform.position;
                 extending = false;
                 retracting = true;
-                if(hook_shot == true)
-                {
-                    last_position = col.gameObject.transform.position;
-                    grabbed_object = col.gameObject;
-                }
             }
         }
     }
