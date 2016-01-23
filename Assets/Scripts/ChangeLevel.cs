a2c2f4f3d02b44b0b74637c6adc330603fd84172
Elit3d <kyle.j.t@live.co.uk>
2016-01-23 00:51:43 +0000

    ChangeLevel script with fade in and fade out
    Changes the level with an included fade out and fade in feature

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    IEnumerator FadeOutLevel(string nextLevelName)
+    {
+        FadeOut();
+        yield return new WaitForSeconds(1.5f);
+        Application.LoadLevel(nextLevelName);
+    }
+
+    public void FadeOut()
+    {
+        animator.Play("FadeOut");
+    }
+
+    public void FadeIn()
+    {
+        animator.Play("FadeIn");
+    }
+
+    public void ChangeToLevel(string nextLevelName)
+    {
+        StartCoroutine(FadeOutLevel(nextLevelName));
+    }
+}
