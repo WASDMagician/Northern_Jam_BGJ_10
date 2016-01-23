b6add938e9010f317405e2c2f067f5ca4134f2b8
WASDMagician <WASDMagician@gmail.com>
2016-01-23 16:56:23 +0000

    .

--- Assets/Scripts/grapple_hook_control.cs
+++ Assets/Scripts/grapple_hook_control.cs
@@ -8,35 +8,89 @@ public class grapple_hook_control : MonoBehaviour {
     public float max_distance;
     public float speed;
     public bool has_fired;
+    public float snap_distance;
+    bool extending;
+    bool retracting;
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
         has_fired = false;
+        extending = false;
+        retracting = false;
     }
 
     void Update()
     {
         if(Input.GetButton("Fire1"))
         {
-            has_fired = true;
+            Fire();
         }
 
-        if(has_fired)
+        if (has_fired)
         {
             Move();
         }
     }
 
+    public void Fire()
+    {
+        if (has_fired == false)
+        {
+            has_fired = true;
+            extending = true;
+            retracting = false;
+        }
+    }
+
     void Move()
     {
+        float distance = Vector3.Distance(transform.position, transform.parent.position);
+        
+        Vector3 initial = transform.position;
+        Vector3 move_by = initial;
+        Vector3 end_position = initial;
 
-        print(Vector3.Distance(transform.position, transform.parent.position));
-        //body.MovePosition(transform.forward * (speed * Time.deltaTime));
-        Vector3 trans = transform.parent.transform.position;
-        trans += transform.forward * (speed * Time.deltaTime);
-        transform.position += trans;
-        print(Vector3.Distance(transform.position, transform.parent.position));
+        if (extending == true && distance < max_distance)
+        {
+            print("Extending");
+            move_by = transform.forward * (speed * Time.deltaTime);
+            end_position = initial + move_by;
+        }
+        else if(extending == true && distance >= max_distance)
+        {
+            print("Switching");
+            extending = false;
+            retracting = true;
+        }
+        else if(retracting == true && distance > snap_distance)
+        {
+            print("Retracting");
+            move_by = -transform.forward * (speed * Time.deltaTime);
+            end_position = initial + move_by;
+        }
+
+        transform.position = end_position;
+
+        if(retracting == true && distance <= snap_distance)
+        {
+            extending = false;
+            retracting = false;
+            has_fired = false;
+            transform.position = transform.parent.transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(!col.CompareTag("Gun"))
+        {
+            if(extending == true)
+            {
+                extending = false;
+                retracting = true;
+            }
+        }
     }
 
 
