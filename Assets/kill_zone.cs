0e3630a62fc5c869d2817c534c0bfc8e67e1631c
WASDMagician <WASDMagician@gmail.com>
2016-01-24 16:52:31 +0000

    added killzone to levels, moved gun

---
+++ Assets/kill_zone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kill_zone : MonoBehaviour {
+    public GameObject spawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.transform.position = GameObject.FindGameObjectWithTag("Spawn_Point").transform.position;
+    }
+}
