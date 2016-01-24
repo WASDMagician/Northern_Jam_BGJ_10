088771b2b50f405516a071852ee7d5577c352448
WASDMagician <WASDMagician@gmail.com>
2016-01-24 15:15:58 +0000

    grappable objects now pulse

--- Assets/pulse.cs
+++ Assets/pulse.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class pulse : MonoBehaviour {
 
+    Renderer renderer;
+    Material material;
+
 	// Use this for initialization
 	void Start () {
-	
+        renderer = GetComponent<Renderer>();
+        material = renderer.material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    float emission = Mathf.PingPong(Time.time, 1.0f);
+        Color base_color = material.color;
+
+        Color final_color = base_color * Mathf.LinearToGammaSpace(emission);
+
+        material.SetColor("_EmissionColor", final_color);
 	}
 }
