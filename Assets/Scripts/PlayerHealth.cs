cfde0cc4d4130c3ae6e0d374343b88bb1c1122cc
WASDMagician <WASDMagician@gmail.com>
2016-01-24 20:00:51 +0000

    m

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
     public int health;
+    private Text health_display;
     
 	void Start () {
         health = 100;
+        health_display = GameObject.FindGameObjectWithTag("Health").GetComponent<Text>();
+        health_display.text = "Health: " + health.ToString();
+
 	}
 	
     public int getHealth()
@@ -22,5 +27,6 @@ public class PlayerHealth : MonoBehaviour {
     public void damage(int d)
     {
         health -= d;
+        health_display.text = "Health: " + health.ToString();
     }
 }
