26d7e5ed258ac3e7723b7a4282c543ea24450ab3
Elit3d <kyle.j.t@live.co.uk>
2016-01-24 09:03:39 +0000

    better crosshair implementation

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
 
+    public Texture2D crosshair;
+
 	public grapple_hook_control grapple_hook;
 
 	//mouse movement
@@ -153,4 +155,11 @@ public class Player_Controller : MonoBehaviour {
 		
 		
 	}
+
+    void OnGUI()
+    {
+        float x = (Screen.width / 2) - (crosshair.width / 2);
+        float y = (Screen.height / 2) - (crosshair.height / 2);
+        GUI.DrawTexture(new Rect(x, y, crosshair.width, crosshair.height), crosshair);
+    }
 }
