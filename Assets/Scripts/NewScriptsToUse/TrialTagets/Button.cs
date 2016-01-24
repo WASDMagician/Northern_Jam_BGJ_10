1835e27e67d0d0fcd82115bfad89efcc4d8909c0
Lordmatics <niallmaple@yahoo.co.uk>
2016-01-24 14:06:58 +0000

    fix

---
+++ Assets/Scripts/NewScriptsToUse/TrialTagets/Button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public SlidingPlatform slidingPlatform;
+
+    private bool isActivated;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("GrappleHook") && isActivated == false)
+        {
+            isActivated = true;
+            Renderer rend = GetComponent<Renderer>();
+            rend.material.color = Color.black;
+            slidingPlatform.Activate(isActivated);
+        }
+    }
+}
